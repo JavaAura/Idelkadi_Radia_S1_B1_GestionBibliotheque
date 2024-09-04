358bbf0af97e631ff4365cf3e631a5dec01fb8f6
radia idelkadi <idelkadiradia@gmail.com>
2024-09-04 19:23:35 +0100

    ajouter lien du jira

--- src/presentation/ConsoleUI.java
+++ src/presentation/ConsoleUI.java
@@ -41,7 +41,7 @@ public class ConsoleUI {
 		} while (choix != 6);
 	}
 
-	//Affiche le menu principal à l'utilisateur
+	// Affiche le menu principal à l'utilisateur
 	private static void afficherMenu() {
 		System.out.println("\nMenu:");
 		System.out.println("1. Ajouter un document");
@@ -52,8 +52,8 @@ public class ConsoleUI {
 		System.out.println("6. Quitter");
 		System.out.print("Veuillez entrer votre choix (1-6) : ");
 	}
-	
-    // Permet d'ajouter un document (Livre ou Magazine) à la bibliothèque.
+
+	// Permet d'ajouter un document (Livre ou Magazine) à la bibliothèque.
 
 	private static void ajouterDocument() {
 		System.out.println("Quel type de Document que vous voulez inserer? (1-Livre , 2-Magazine)");
@@ -69,10 +69,16 @@ public class ConsoleUI {
 		System.out.println("Auteur:");
 		String auteur = scanner.nextLine();
 
-		System.out.print("Date de publication (YYYY-MM-DD) : ");
-		String dateStr = scanner.nextLine();
-		LocalDate datePublication = DateUtils.convertStringToDate(dateStr);
+		LocalDate datePublication = null;
+		while (datePublication == null) { // Boucle jusqu'à obtenir une date valide
+			System.out.print("Date de publication (YYYY-MM-DD) : ");
+			String dateStr = scanner.nextLine();
+			datePublication = DateUtils.convertStringToDate(dateStr);
 
+			if (datePublication == null) {
+				System.out.println("Erreur : format de date invalide. Utilisez 'yyyy-MM-dd'.");
+			}
+		}
 		System.out.println("Nombre de pages:");
 		int nbrpages = scanner.nextInt();
 
@@ -98,38 +104,38 @@ public class ConsoleUI {
 
 	}
 
-	//emprunter un document en fonction de son titre
+	// emprunter un document en fonction de son titre
 	private static void emprunterDocument() {
 		scanner.nextLine();
-        System.out.print("Entrez le titre du document à emprunter : ");
+		System.out.print("Entrez le titre du document à emprunter : ");
 		String titreDoc = scanner.nextLine();
 		bibliotheque.EmprunterDocument(titreDoc);
 	}
 
-	//retourner un document en fonction de son titre
+	// retourner un document en fonction de son titre
 	private static void retournerDocument() {
 		scanner.nextLine();
-        System.out.print("Entrez le titre du document à retourner : ");
+		System.out.print("Entrez le titre du document à retourner : ");
 		String titreDoc = scanner.nextLine();
 		bibliotheque.RetournerDocument(titreDoc);
 	}
 
-	//Affiche tous les documents
+	// Affiche tous les documents
 	private static void afficherTousLesDocuments() {
 		bibliotheque.AfficherTousDocuments();
 	}
 
-	//Recherche un document par son titre et affiche ses détails s'il est trouvé
+	// Recherche un document par son titre et affiche ses détails s'il est trouvé
 	private static void rechercherDocument() {
 		scanner.nextLine();
-        System.out.print("Entrez le titre du document à rechercher : ");
+		System.out.print("Entrez le titre du document à rechercher : ");
 		String titreDoc = scanner.nextLine();
 
 		Document docRecherche = bibliotheque.RechercherDocument(titreDoc);
 
 		if (docRecherche != null) {
 			docRecherche.afficherDetails();
-		}else {
+		} else {
 			System.out.println("Document introuvable.");
 		}
 	}
